df1b775a85149ca282c70d09fae20d69f9e5def0
Sean Demura <sean@apcera.com>
2018-05-08 12:10:36 -0400

    more commits

--- flake.sh
+++ flake.sh
@@ -2,4 +2,9 @@
 
 export FLAKE="$((1 + RANDOM % 10))"
 
-[[ "$FLAKE" -eq "$FAIL" ]] || exit 1
+echo "testing if FLAKE == FAIL"
+if [[ "$FLAKE" -eq "$FAIL" ]]; then
+    echo "FLAKE equals FAIL"
+    echo "exiting"
+    exit 1
+fi
